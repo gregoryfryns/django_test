44c229e9579ce6504f76fd9730a1bd576c638719
Gregory Fryns <gregory.fryns@gmail.com>
2015-04-17 13:14:10 -0700

    uploaded files handling

--- imageconv/models.py
+++ imageconv/models.py
@@ -6,4 +6,12 @@ from django.db import models
 
 # Create your models here.
 class Image(models.Model):
-    img_file = models.ImageField(upload_to='uploaded_images/%Y/%m/%d')
+    def __init__(self, image_file):
+        self.img_file = image_file
+
+    def get_size(self):
+        return self.img_file.size
+
+    def save(self):
+        path = default_storage.save('uploaded_images/', ContentFile(self.img_file.read())) 
+#    img_file = models.ImageField(upload_to='uploaded_images/%Y/%m/%d')
--- imageconv/views.py
+++ imageconv/views.py
@@ -6,21 +6,20 @@ from django.core.urlresolvers import reverse
 from imageconv.models import Image
 from imageconv.forms import FileUploadForm
 
-import os
-from django.core.files.storage import default_storage
-from django.core.files.base import ContentFile
 from django.conf import settings
 
 def list(request):
     # Handle file upload
+    image = None
+    error_msg = None
     if request.method == 'POST':
         form = FileUploadForm(request.POST, request.FILES)
         if form.is_valid():
-            image = request.FILES['upload_image']
-            path = default_storage.save('uploaded_images/%Y/%m/%d', ContentFile(image.read())) 
-            tmp_file = os.path.join(settings.MEDIA_ROOT, path)
-#            image = Image(request.FILES['upload_image'])
-#            image.save()
+            image = Image(form.cleaned_data['upload_image'])
+            if image.get_size() <= int(settings.MAX_UPLOAD_SIZE):
+                image.save()
+            else:
+                error_msg = "This file is too big! Please upload a file smaller than " + sizeof_fmt(int(settings.MAX_UPLOAD_SIZE))
 
             # Redirect to the document list after POST
             return HttpResponseRedirect(reverse('imageconv:list'))
@@ -28,12 +27,19 @@ def list(request):
         form = FileUploadForm() # A empty, unbound form
 
     # Load images for the list page
-#    images = Image.objects.all()
     images = None
 
     # Render list page with the documents and the form
     return render_to_response(
         'imageconv/list.html',
-        {'images': images, 'form': form, 'coucou': "no coucou"},
+        {'image': image, 'form': form, 'error_msg': error_msg, 'max_size': sizeof_fmt(int(settings.MAX_UPLOAD_SIZE))},
         context_instance=RequestContext(request)
     )
+
+def sizeof_fmt(num, suffix='B'):
+    """Takes a size (number of units) and the unit as an input and returns a human readable string"""
+    for unit in ['','K','M','G','T','P','E','Z']:
+        if abs(num) < 1024.0:
+            return "%3.1f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, 'Yi', suffix)
