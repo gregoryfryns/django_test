99fb689c20dd7378917001b9087717c6ea8fc550
Gregory Fryns <gregory.fryns@gmail.com>
2015-04-22 13:29:34 -0700

    Refactored Weather app - ready to use with other apis

--- weather/models.py
+++ weather/models.py
@@ -2,21 +2,65 @@ from django.db import models
 import urllib, json, datetime, sys
 
 # Create your models here.
-class Weather:
+class AbstractWeatherReport(object):
+    """Abstract class for weather reports. Assumes that correct zip code and country code are passed to the initializer"""
     def __init__(self, zipcode, country_code):
-        url = "http://api.openweathermap.org/data/2.5/weather?zip=" + zipcode + "," + country_code
+        self.zipcode = zipcode
+        self.country_code = country_code
+        self.error_msg = None
+        self.location = ""
+        self.temperature_kelvin = 0
+        self.iconUrl = ""
+        self.description = ""
+
+    def has_error(self):
+        return not self.error_msg == None
+
+    def get_error_msg(self):
+        return self.error_msg
+
+    def get_location(self):
+        return self.location
+
+    def get_temperature_kelvin(self):
+        return self.temperature_kelvin
+
+    def get_temperature_celsius(self):
+        return self.temperature_kelvin - 273.15
+
+    def get_temperature_fahrenheit(self):
+        return ((9.0/5.0) * self.get_temperature_celsius()) + 32
+
+    def get_icon_url(self):
+        return self.iconUrl
+
+    def get_description(self):
+        return self.description
+
+    def update(self):
+        raise NotImplementedError("Method not implemented")
+
+
+class OpenWeatherMapReport(AbstractWeatherReport):
+    """Implementation of the AbstractWeatherReport class using the Open Weather Map api: http://openweathermap.org/api"""
+    def __init__(self, zipcode, country_code):
+        super(OpenWeatherMapReport, self).__init__(zipcode, country_code)
+        self.service_base_url = "http://api.openweathermap.org/data/2.5/weather?zip=%s,%s"
+        self.icon_base_url = "http://openweathermap.org/img/w/%s.png"
+
+    def update(self):
+        url = self.service_base_url % (self.zipcode, self.country_code)
         try:
             response = urllib.urlopen(url);
             data = json.loads(response.read())
         except:
-            self.error_msg = "Could not connect to the weather report service, please try again!"
+            self.error_msg = "Could not connect to http://api.openweathermap.org/, please try again!"
         else:
             if data["cod"] != 200:
-                self.error_msg = "Could not load weather report for " + zipcode + ", " + country_code
+                self.error_msg = data["cod"] + " - " + data["message"]
             else:
                 self.location = data["name"]
-                self.temperature_celsius = float(data["main"]["temp"]) - 273.15
-                self.temperature_fahrenheit = ((9.0/5.0) * self.temperature_celsius) + 32
-                self.iconId = data["weather"][0]["icon"]
+                self.temperature_kelvin = float(data["main"]["temp"])
+                self.iconUrl = self.icon_base_url % (data["weather"][0]["icon"])
                 self.description = data["weather"][0]["description"]
 
--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from weather.models import Weather
+from weather.models import AbstractWeatherReport, OpenWeatherMapReport
 
 # Create your views here.
 def weather(request):
@@ -10,7 +10,16 @@ def weather(request):
         zipcode = "94025"
         country_code = "US"
 
-    weather_report = Weather(zipcode, country_code)
-    context = {'weather_report': weather_report}
+    weather_report = OpenWeatherMapReport(zipcode, country_code)
+    weather_report.update()
+
+    error_msg = weather_report.get_error_msg()
+    location = weather_report.get_location()
+    temp_celsius = str(weather_report.get_temperature_celsius())
+    temp_fahrenheit = str(weather_report.get_temperature_fahrenheit())
+    icon_url = weather_report.get_icon_url()
+    description = weather_report.get_description()
+
+    context = {'error_msg': error_msg, 'location': location, 'temp_celsius': temp_celsius,'temp_fahrenheit': temp_fahrenheit, 'icon_url': icon_url, 'description': description}
     return render(request, 'weather/index.html', context)
 
